9cfb0e2285a6ca050d9a784ed85ea99655f2ec99
HarenaFiantso <fiantsorav@gmail.com>
2023-12-07 19:24:54 +0300

    fix: Fix the SQL script for creating account table

--- src/main/resources/create_table_account.sql
+++ src/main/resources/create_table_account.sql
@@ -1,21 +1,9 @@
 -- Create the account table if not exists
+-- Should create account type and currency first
 CREATE TABLE IF NOT EXISTS account (
     account_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    balance DECIMAL(15, 2) NOT NULL CHECK (balance >= 0),
-    currency_id INT NOT NULL,
-    FOREIGN KEY (currency_id) REFERENCES currencies(currency_id)
+    account_name VARCHAR(100) NOT NULL,
+    balance DOUBLE PRECISION NOT NULL,
+    currency_id INT  REFERENCES currency(currency_id) NOT NULL,
+    account_type "account_type" NOT NULL,
 );
--- Three examples of mock data
-INSERT INTO account (
-        first_name,
-        last_name,
-        password,
-        balance,
-        currency_id
-    )
-VALUES ('John', 'Doe', 'password1', 1500, 1),
-    ('Alice', 'Smith', 'password2', 500, 2),
-    ('Bob', 'Johnson', 'password3', 7500, 3);
