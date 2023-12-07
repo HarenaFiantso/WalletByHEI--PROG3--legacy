76accabbe0c311d641ceecb49c1bdf7239fea4ed
HarenaFiantso <fiantsorav@gmail.com>
2023-12-07 18:37:18 +0300

    chore: Refactor the Account entity and add a new attribute to currency

--- src/main/java/com/walletbyhei/model/Account.java
+++ src/main/java/com/walletbyhei/model/Account.java
@@ -1,16 +1,23 @@
 package com.walletbyhei.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
     /* - Use camelCase as variable notation
-    *  - Add some additional attributs
-    *  - TODO: Create an Enum file for the type attribut
+    *  - Add some additional attributes
+    *  - TODO: Create an Enum file for the accountType attribute
     *  */
     private Long accountId;
     private String accountName;
     private double balance;
     private List<?> transactionList;
-    private String currency;
-    private String type;
+    private int currencyId;
+    private String accountType;
 }
---
+++ src/main/java/com/walletbyhei/model/Currency.java
@@ -0,0 +1,17 @@
+package com.walletbyhei.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Currency {
+    /* - Use camelCase as variable notation
+    *  - TODO: Create an Enum file for the currencyCode attribute
+    *  */
+    private Long currencyId;
+    private String currencyName;
+    private String currencyCode;
+}
