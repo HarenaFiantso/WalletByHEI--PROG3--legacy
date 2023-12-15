5b00115331a63d18027553c3a8e30b169b95f495
andyDevMdg <andyavotiana@gmail.com>
2023-12-15 14:52:41 +0300

    feat: Add the entity Category

---
+++ src/main/java/com/walletbyhei/model/Category.java
@@ -0,0 +1,13 @@
+package com.walletbyhei.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+}
