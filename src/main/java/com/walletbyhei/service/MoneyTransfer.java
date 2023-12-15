0928b26554e5d19b76069ab17dbea95728eabede
Fiantso Harena <fiantsorav@gmail.com>
2023-12-15 05:50:50 +0300

    test: Create a test

--- src/main/java/com/walletbyhei/service/MoneyTransfer.java
+++ src/main/java/com/walletbyhei/service/MoneyTransfer.java
@@ -3,10 +3,17 @@ package com.walletbyhei.service;
 import com.walletbyhei.model.Account;
 import com.walletbyhei.model.Transaction;
 import com.walletbyhei.model.type.TransactionType;
+import com.walletbyhei.repository.crudOperationsImpl.CurrencyValueRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class MoneyTransfer {
+  CurrencyValueRepository currencyValueRepository = new CurrencyValueRepository();
 
   public void transferMoneyBetweenAccounts(Account fromAccount, Account toAccount, double amount) {
     if (fromAccount.equals(toAccount)) {
@@ -51,5 +58,39 @@ public class MoneyTransfer {
       currentBalance += amount;
     }
     account.getBalance().setAmount(currentBalance);
+  }
+
+  public void convertCurrency(Account fromAccount, Account toAccount, double amount, LocalDate date) {
+    if (fromAccount.equals(toAccount)) {
+      throw new IllegalArgumentException("Cannot transfer money to the same account.");
+    }
+
+    if (!fromAccount.getCurrency().equals(toAccount.getCurrency())) {
+      double conversionRate = currencyValueRepository.getConversionRate(fromAccount.getCurrency().getCurrencyId(), toAccount.getCurrency().getCurrencyId(), date);
+
+      if (conversionRate == 0) {
+        throw new RuntimeException("Conversion rate not found for given currencies and date.");
+      }
+
+      double convertedAmount = amount * conversionRate;
+
+      Transaction debitTransaction = createDebitTransaction(fromAccount, amount);
+      Transaction creditTransaction = createCreditTransaction(toAccount, convertedAmount);
+
+      fromAccount.getTransactionList().add(debitTransaction);
+      toAccount.getTransactionList().add(creditTransaction);
+
+      updateAccountBalance(fromAccount, amount, TransactionType.DEBIT);
+      updateAccountBalance(toAccount, convertedAmount, TransactionType.CREDIT);
+    } else {
+      Transaction debitTransaction = createDebitTransaction(fromAccount, amount);
+      Transaction creditTransaction = createCreditTransaction(toAccount, amount);
+
+      fromAccount.getTransactionList().add(debitTransaction);
+      toAccount.getTransactionList().add(creditTransaction);
+
+      updateAccountBalance(fromAccount, amount, TransactionType.DEBIT);
+      updateAccountBalance(toAccount, amount, TransactionType.CREDIT);
+    }
   }
 }
--- src/test/java/testConnectionToDb/testService/MoneyTransferIT.java
+++ src/test/java/testConnectionToDb/testService/MoneyTransferIT.java
@@ -2,10 +2,14 @@ package testConnectionToDb.testService;
 
 import com.walletbyhei.model.Account;
 import com.walletbyhei.model.Balance;
+import com.walletbyhei.model.Currency;
 import com.walletbyhei.model.Transaction;
 import com.walletbyhei.model.type.TransactionType;
+import com.walletbyhei.repository.crudOperationsImpl.CurrencyValueRepository;
 import com.walletbyhei.service.MoneyTransfer;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.mockito.Mockito.*;
 
@@ -40,5 +44,37 @@ public class MoneyTransferIT {
 
     verify(moneyTransfer).updateAccountBalance(fromAccount, 200.0, TransactionType.DEBIT);
     verify(moneyTransfer).updateAccountBalance(toAccount, 200.0, TransactionType.CREDIT);
+  }
+
+  /* TODO: Need a better code testing */
+  @Test
+  public void convertCurrencyTest() {
+    Account fromAccount = mock(Account.class);
+    Account toAccount = mock(Account.class);
+
+    CurrencyValueRepository currencyValueRepository = mock(CurrencyValueRepository.class);
+
+    double amount = 100.0;
+    LocalDate date = LocalDate.of(2023, 12, 6);
+    double conversionRate = 4600.0;
+
+    when(fromAccount.equals(toAccount)).thenReturn(false);
+
+    Currency fromCurrency = new Currency(1L, "Euro", "EUR");
+    Currency toCurrency = new Currency(2L, "Ariary", "ARI");
+    when(fromAccount.getCurrency()).thenReturn(fromCurrency);
+    when(toAccount.getCurrency()).thenReturn(toCurrency);
+
+    when(currencyValueRepository.getConversionRate(fromCurrency.getCurrencyId(), toCurrency.getCurrencyId(), date))
+        .thenReturn(conversionRate);
+
+    MoneyTransfer moneyTransfer = new MoneyTransfer();
+    moneyTransfer.convertCurrency(fromAccount, toAccount, amount, date);
+
+    verify(fromAccount).getTransactionList().add(any(Transaction.class));
+    verify(toAccount).getTransactionList().add(any(Transaction.class));
+
+    verify(fromAccount).getBalance().setAmount(0.00);
+    verify(toAccount).getBalance().setAmount(amount * conversionRate);
   }
 }
