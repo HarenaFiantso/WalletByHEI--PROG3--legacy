c18f72a2ad8deea1e002162d0ae3788298411094
andyDevMdg <andyavotiana@gmail.com>
2023-12-15 17:41:06 +0300

    refactor: refactor the code for the function to get the sum of the debit and credit

--- src/main/resources/functions/get_debit_credit_sum.sql
+++ src/main/resources/functions/get_debit_credit_sum.sql
@@ -1,10 +1,10 @@
+-- Create a function to get the sum of amounts for "CREDIT" and "DEBIT" transaction types within a date range
 CREATE OR REPLACE FUNCTION get_debit_credit_sum(
     selected_account_id INT,
     start_datetime TIMESTAMP,
     end_datetime TIMESTAMP
-)
-    RETURNS TABLE (transaction_type VARCHAR, total_sum DECIMAL)
-    LANGUAGE PLPGSQL
+) RETURNS TABLE (transaction_type VARCHAR, total_sum DOUBLE PRECISION)
+    LANGUAGE plpgsql
 AS
 $$
 BEGIN
@@ -13,21 +13,22 @@ BEGIN
             'CREDIT'::VARCHAR AS transaction_type,
             COALESCE(SUM(t.amount), 0) AS total_sum
         FROM
-            transaction t
+            "transaction" t
         WHERE
-                t.account_id = selected_account_id
-          AND t.transaction_type = 'CREDIT'
-          AND t.transaction_date_time BETWEEN start_datetime AND end_datetime;
+                t.account_id = selected_account_id AND
+                t.transaction_type = 'CREDIT' AND
+            t.transaction_date_time BETWEEN start_datetime AND end_datetime
 
-    RETURN QUERY
+        UNION ALL
+
         SELECT
             'DEBIT'::VARCHAR AS transaction_type,
             COALESCE(SUM(t.amount), 0) AS total_sum
         FROM
-            transaction t
+            "transaction" t
         WHERE
-                t.account_id = selected_account_id
-          AND t.transaction_type = 'DEBIT'
-          AND t.transaction_date_time BETWEEN start_datetime AND end_datetime;
+                t.account_id = selected_account_id AND
+                t.transaction_type = 'DEBIT' AND
+            t.transaction_date_time BETWEEN start_datetime AND end_datetime;
 END;
 $$;
