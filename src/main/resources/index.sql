841f88aa97a404e8b61b7d70ea5a3de8aac76a1d
andyDevMdg <andyavotiana@gmail.com>
2023-12-15 16:41:14 +0300

    feat: Add SQL idempotent script to add category_id column in transaction table

--- src/main/resources/index.sql
+++ src/main/resources/index.sql
@@ -65,6 +65,31 @@ CREATE TABLE IF NOT EXISTS "transaction"
     FOREIGN KEY (account_id) REFERENCES account (account_id)
 );
 
+-- Add category_id column to the "transaction" table if it doesn't exist
+DO $$
+    BEGIN
+        BEGIN
+            ALTER TABLE "transaction"
+                ADD COLUMN category_id INT NOT NULL;
+        EXCEPTION
+            WHEN duplicate_column THEN
+            -- Ignore if the column already exists
+        END;
+    END $$;
+
+-- Add foreign key constraint if it doesn't exist
+DO $$
+    BEGIN
+        BEGIN
+            ALTER TABLE "transaction"
+                ADD CONSTRAINT fk_transaction_category
+                    FOREIGN KEY (category_id) REFERENCES category(category_id);
+        EXCEPTION
+            WHEN duplicate_object THEN
+            -- Ignore if the constraint already exists
+        END;
+    END $$;
+
 -- Creating the table transfer history
 CREATE TABLE IF NOT EXISTS transfer
 (
